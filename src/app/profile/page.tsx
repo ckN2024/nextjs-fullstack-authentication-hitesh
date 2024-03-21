3244ecc12347e8e370029f6c6a2ba42e7093c6f6
ck <ck@tyloones.com>
2024-03-21 16:30:02 +0530

    added middleware for protected routes + me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,9 +2,11 @@
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import React from "react";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = React.useState("")
 
   const onLogout = async () => {
     try {
@@ -15,15 +17,30 @@ export default function ProfilePage() {
     }
   };
 
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me')
+    console.log(res.data);
+    setData(res.data.data._id)
+  }
+
   return (
     <div>
       <h1>Profile Page</h1>
+      <h2>{data==="" ? "No DATA": (
+        <Link href={`profile/${data}`}>
+          {data}
+        </Link>
+      )}</h2>
       <hr />
       <p>This is the profile page</p>
       <hr />
 
       <button onClick={onLogout} className="border bg-blue-500">
         Log Out
+      </button>
+
+      <button onClick={getUserDetails} className="border bg-green-500">
+        Get user details
       </button>
     </div>
   );
